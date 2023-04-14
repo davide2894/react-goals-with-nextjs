12aebe4f1103bcd78075d2d998b17fed60d6e9c9
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2023-04-14 16:25:38 +0200

    feat: add links to pages in home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,63 +1,42 @@
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { Inter } from "next/font/google";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useReducer } from "react";
 import { useDispatch } from "react-redux";
 import { login } from "../redux/slices/userSlice";
 import Goals from "@components/goals/Goals";
 import MyAccount from "@components/myAccount/MyAccount";
-
-const inter = Inter({ subsets: ["latin"] });
+import router, { useRouter } from "next/router";
+import Layout from "@components/layout/Layout";
+import useCheckUser from "@utils/useCheckUser";
+import Link from "next/link";
 
 export default function Home() {
-  const [isUserLogged, setIsUserLogged] = useState<undefined | Boolean>(
-    undefined
-  );
-  const dispatch = useDispatch();
+  const router = useRouter();
+  const checkUser = useCheckUser();
 
-  useEffect(() => {
-    const auth = getAuth();
-    onAuthStateChanged(auth, (user) => {
-      console.log("onAuthStateChanged");
-      if (user) {
-        setIsUserLogged(true);
-        dispatch(
-          login({
-            email: user.email !== null ? user.email : "",
-            uid: user.uid,
-            userDocId: `${user.email}-${user.uid}`,
-          })
-        );
-      } else {
-        setIsUserLogged(false);
-      }
-    });
+  let content;
 
-    return () => {
-      console.log("onAuthStateChanged return fn");
-    };
-  }, [dispatch]);
-
-  let content:
-    | string
-    | number
-    | boolean
-    | JSX.Element
-    | React.ReactFragment
-    | null
-    | undefined;
-
-  if (typeof isUserLogged !== "undefined") {
-    if (isUserLogged) {
-      content = <Goals />;
+  if (checkUser.isUserLogged !== null) {
+    if (checkUser.isUserLogged) {
+      content = (
+        <div>
+          <Link href={"/goals"}>Go to Goals page</Link>
+        </div>
+      );
     } else {
       content = (
-        <div className="home__welcomeMessage">
-          <h1>WELCOME TO DAILY GOAL TRACKER</h1>
-          <MyAccount />
+        <div>
+          <Link href={"/signin"}>Sign in</Link>
         </div>
       );
     }
+  } else {
+    content = <div>Retrieving info...</div>;
   }
 
-  return <div className="home">{content}</div>;
+  return (
+    <Layout>
+      <div className="home">{content}</div>
+    </Layout>
+  );
 }
