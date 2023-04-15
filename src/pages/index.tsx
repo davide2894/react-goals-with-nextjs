181fb5cd5652fe36ee647dccb1b5d0569e9a8f0c
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2023-04-15 16:01:04 +0200

    fix(homepage): use user context provided by next-firebase-auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,24 @@
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { Inter } from "next/font/google";
-import { useState, useEffect, useReducer } from "react";
-import { useDispatch } from "react-redux";
-import { login } from "../redux/slices/userSlice";
-import Goals from "@components/goals/Goals";
-import MyAccount from "@components/myAccount/MyAccount";
-import router, { useRouter } from "next/router";
 import Layout from "@components/layout/Layout";
-import useCheckUser from "@utils/useCheckUser";
 import Link from "next/link";
+import { useAuthUser, withAuthUser } from "next-firebase-auth";
 
-export default function Home() {
-  const router = useRouter();
-  const checkUser = useCheckUser();
+function Home() {
+  const user = useAuthUser();
 
   let content;
 
-  if (checkUser.isUserLogged !== null) {
-    if (checkUser.isUserLogged) {
-      content = (
-        <div>
-          <Link href={"/goals"}>Go to Goals page</Link>
-        </div>
-      );
-    } else {
-      content = (
-        <div>
-          <Link href={"/signin"}>Sign in</Link>
-        </div>
-      );
-    }
+  if (user.id) {
+    content = (
+      <div>
+        <Link href={"/goals"}>Go to Goals page</Link>
+      </div>
+    );
   } else {
-    content = <div>Retrieving info...</div>;
+    content = (
+      <div>
+        <Link href={"/signin"}>Sign in</Link>
+      </div>
+    );
   }
 
   return (
@@ -40,3 +27,5 @@ export default function Home() {
     </Layout>
   );
 }
+
+export default withAuthUser({})(Home);
