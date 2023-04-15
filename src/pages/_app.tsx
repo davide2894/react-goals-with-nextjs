17c1b400d3507c6b843402e3bc2570c3d4cd3524
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2023-04-15 11:35:38 +0200

    feat(next-firebase-auth): init config in _app component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,11 @@ import "@styles/globals.css";
 import type { AppProps } from "next/app";
 import { store } from "../redux/store";
 import { Provider } from "react-redux";
+import initAuth from "@utils/initAuth";
 
 export default function App({ Component, pageProps }: AppProps) {
+  initAuth();
+
   return (
     <Provider store={store}>
       <Component {...pageProps} />
