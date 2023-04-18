4099816148edcc129789b4630b211ecce813493e
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2023-04-18 17:47:09 +0200

    style: layout with tailwind css

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,9 +1,7 @@
-import React from "react";
-
 function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div>
-      <div className="home__welcomeMessage">
+      <div>
         <h1 className="text-2xl font-bold mt-16 text-center">
           WELCOME TO DAILY GOAL TRACKER
         </h1>
@@ -11,7 +9,7 @@ function Layout({ children }: { children: React.ReactNode }) {
 
       <div>{children}</div>
 
-      <footer className="absolute text-lg mt-8">
+      <footer className="absolute bottom-1 text-lg mt-12">
         <p>Made with determination and ☕ by Davide Iaiunese</p>
       </footer>
     </div>
