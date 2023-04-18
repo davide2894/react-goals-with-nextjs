bc82f7939a04e3ef7985b92bbfce1e3ed6fc5213
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2023-04-18 17:59:40 +0200

    feat(home): check user on server side and decide what link to render

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,42 @@
-import Layout from "@components/layout/Layout";
 import Link from "next/link";
-import { useAuthUser, withAuthUser } from "next-firebase-auth";
+import { withAuthUserTokenSSR } from "next-firebase-auth";
 
-function Home() {
-  const user = useAuthUser();
-
-  let content;
-  let route;
-  let text;
-
-  if (user.id) {
-    route = "/goals";
-    text = "Go to Goals page";
-  } else {
-    route = "/signin";
-    text = "Sign in";
-  }
-
-  content = (
-    <div className="flex justify-center">
-      <Link
-        className="self-center bg-yellow-500 hover:bg-yellow-700 text-black font-bold py-2 px-4 rounded"
-        href={route}>
-        {text}
-      </Link>
+function Home(props: { route: string; text: string }) {
+  return (
+    <div className="home">
+      <div className="flex justify-center mt-10">
+        <Link
+          className="self-center bg-yellow-500 hover:bg-yellow-700 text-black font-bold py-2 px-4 rounded"
+          href={props.route}>
+          {props.text}
+        </Link>
+      </div>
     </div>
   );
-
-  return <div className="home">{content}</div>;
 }
 
-export default withAuthUser({})(Home);
+export default Home;
+
+export const getServerSideProps = withAuthUserTokenSSR({})(
+  async ({ AuthUser }) => {
+    const user = AuthUser;
+
+    let route;
+    let text;
+
+    if (user.id) {
+      route = "/goals";
+      text = "Go to Goals page";
+    } else {
+      route = "/signin";
+      text = "Sign in";
+    }
+
+    return {
+      props: {
+        route,
+        text,
+      },
+    };
+  }
+);
