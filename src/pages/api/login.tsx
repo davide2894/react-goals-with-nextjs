9e2897661adbb714fff8d4daf227ba830e1e6de4
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2023-04-20 19:14:22 +0200

    fix(login api): typescript error linting

--- src/pages/api/login.tsx
+++ src/pages/api/login.tsx
@@ -1,9 +1,10 @@
 import { setAuthCookies } from "next-firebase-auth";
 import initAuth from "@utils/initAuth";
+import { NextApiRequest, NextApiResponse } from "next";
 
 initAuth();
 
-const handler = async (req, res) => {
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     await setAuthCookies(req, res);
   } catch (e) {
