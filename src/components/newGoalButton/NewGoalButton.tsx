bcdce7ed4f7acb8d98e12684ece5b58f442d70f5
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2023-04-27 20:20:20 +0200

    style(new goal button): use icon for plus sign and center it into the button

--- src/components/newGoalButton/NewGoalButton.tsx
+++ src/components/newGoalButton/NewGoalButton.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import GoalForm from "../goalForm/GoalForm";
 import Modal from "@components/modal/Modal";
+import ButtonIcon from "@components/buttonIcon/ButtonIcon";
 
 function NewGoalButton() {
   const [showNewGoalForm, setShowNewGoalForm] = useState(false);
@@ -8,9 +9,9 @@ function NewGoalButton() {
   return (
     <div>
       <button
-        className="fixed flex justify-center right-5 bottom-16 md:bottom-5 w-16 h-16 align-middle rounded-full bg-yellow-500 hover:bg-yellow-700"
+        className="fixed flex flex-col justify-center right-5 bottom-16 md:bottom-5 w-16 h-16 rounded-full bg-yellow-500 hover:bg-yellow-700"
         onClick={() => setShowNewGoalForm(true)}>
-        <p className="text-black text-center text-5xl font-bold">+</p>
+        <ButtonIcon iconName="add-icon" customCssClasses="ml-auto mr-auto" />
       </button>
       {showNewGoalForm && (
         <Modal mode="add" onClose={() => setShowNewGoalForm(false)}>
