6fc7db43d470c72131552b584f4d00c86f2b81e9
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2023-04-27 19:06:01 +0200

    style: add button now has an icon so it can be centered more easly

--- src/components/buttonIcon/ButtonIcon.tsx
+++ src/components/buttonIcon/ButtonIcon.tsx
@@ -1,7 +1,18 @@
-function ButtonIcon(props: { iconName: string }) {
+function ButtonIcon(props: { iconName: string; customCssClasses?: string }) {
+  let defaultClasses =
+    "block w-[25px] h-[25px] bg-no-repeat bg-center bg-contain hover:scale-150 group-disabled:pointer-events-none group-disabled:opacity-50 hover:cursor-pointer";
+
+  let componentCssClasses: string;
+
+  if (props.customCssClasses) {
+    componentCssClasses = `${defaultClasses} ${props.customCssClasses}`;
+  } else {
+    componentCssClasses = defaultClasses;
+  }
+
   return (
     <span
-      className="block w-[25px] h-[25px] bg-no-repeat bg-center bg-contain hover:scale-150 group-disabled:pointer-events-none group-disabled:opacity-50 hover:cursor-pointer"
+      className={componentCssClasses}
       style={{
         backgroundImage: `url("./${props.iconName}.svg")`,
       }}></span>
