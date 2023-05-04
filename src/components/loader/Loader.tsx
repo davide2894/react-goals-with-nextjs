030d236aff0fe2e01c7c9db4d076afa732e800d4
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2023-05-04 22:07:03 +0200

    style: loader component

--- src/components/loader/Loader.tsx
+++ src/components/loader/Loader.tsx
@@ -1,5 +1,9 @@
 function Loader() {
-  return <div className="flex justify-center mt-8">Loading...</div>;
+  return (
+    <div className="h-screen flex flex-col justify-center ml-auto mr-auto mb-4 text-4xl text-white text-center font-bold absolute top-0 bottom-0 left-0 right-0 bg-transparent backdrop-blur-md z-10">
+      LOADING...
+    </div>
+  );
 }
 
 export default Loader;
