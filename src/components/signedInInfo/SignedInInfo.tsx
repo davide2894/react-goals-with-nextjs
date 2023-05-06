d7300c281253ad30ae5883ffadfabd88454d5615
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2023-05-06 15:35:12 +0200

    refactor(SignedInInfo): remove unused lines and logs

--- src/components/signedInInfo/SignedInInfo.tsx
+++ src/components/signedInInfo/SignedInInfo.tsx
@@ -1,17 +1,6 @@
 import React from "react";
 
 function SignedInInfo(props: { isGuest?: boolean; email: string }) {
-  console.log({ props });
-
-  const dateObj = new Date();
-  const hrs = dateObj.getHours();
-  const mins = dateObj.getMinutes();
-  const seconds = dateObj.getSeconds();
-  const milliseconds = dateObj.getMilliseconds();
-  console.log(
-    `SignedInInfo was rendered at ${hrs}-${mins}-${seconds}-${milliseconds}`
-  );
-
   return (
     <div className="text-right">
       {props.email.includes("reactdailygoaltrackerguestprofile") ? (
@@ -20,26 +9,6 @@ function SignedInInfo(props: { isGuest?: boolean; email: string }) {
         <p>Singed in as {props.email}</p>
       )}
     </div>
-  );
-}
-
-// eslint-disable-next-line no-unused-vars
-function customPropsAreEqual(
-  prevProps: { isGuest: boolean; email: string },
-  nextProps: { isGuest: boolean; email: string }
-) {
-  console.log(
-    "prevProps.isGuest === nextProps.isGuest",
-    prevProps.isGuest === nextProps.isGuest
-  );
-  console.log(
-    "prevProps.email === nextProps.email",
-    prevProps.email === nextProps.email
-  );
-
-  return (
-    prevProps.isGuest === nextProps.isGuest &&
-    prevProps.email === nextProps.email
   );
 }
 
