9f561789a510e98a746c11af386b200a9f6da476
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2023-05-06 15:12:15 +0200

    refactor: remove unused component

--- src/components/errorLogger/ErrorLogger.tsx
+++
@@ -1,9 +0,0 @@
-function ErrorLogger(props: { errorMessage: string }) {
-  return (
-    <div className="errorMessage">
-      <h1>{props.errorMessage}</h1>
-    </div>
-  );
-}
-
-export default ErrorLogger;
