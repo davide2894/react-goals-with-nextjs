3d5a5b2b5bc1baa971350a14a8cb8030da8da335
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2023-05-17 20:48:48 +0200

    refactor(login api): remove logs

--- src/pages/api/login.tsx
+++ src/pages/api/login.tsx
@@ -1,15 +1,12 @@
 import { setAuthCookies } from "next-firebase-auth";
 import initAuth from "@utils/initAuth";
 import { NextApiRequest, NextApiResponse } from "next";
-import { log } from "console";
 
 initAuth();
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    log("login api -> before setAuthCookies");
     await setAuthCookies(req, res);
-    log("login api -> before setAuthCookies");
   } catch (e) {
     // eslint-disable-next-line no-console
     console.error(e);
